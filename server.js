2e7b3f5ce100bb809a448badda5178fd9a22ae97
bsolis6878 <bsolis6878@gmail.com>
2022-09-27 20:37:03 -0500

    added database connection to server

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// express middleware
+// Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World'
-    });
-});
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username
+        user: 'root',
+        // Your MySQL password
+        password: 'uu7o2mz6',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
